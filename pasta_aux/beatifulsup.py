d6e2a325fd31e4f8dc526e4f6fbdb2b7e2d01f0a
Bryan Borges LIma <bryanguilima7912@gmail.com>
2026-07-14 20:45:08 -0300

    Pasta ajeitada

--- pasta_aux/beatifulsup.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys 
-import time
-
-driver = webdriver.Chrome()
-driver.get("http://www.python.com")
-titulo = driver.find_element(By.TAG_NAME, 'title')
-print(titulo.text)
